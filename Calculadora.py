0d7512e85c994c1964bfdb91497939432e498191
Jefferson Whitney <81323940+jeffersonwhitney@users.noreply.github.com>
2022-01-07 14:31:03 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -2,51 +2,37 @@ import PySimpleGUI as sg
 
 class Calculadora:
   def __init__(self):
-    #Layout da Caculadora
+    self.digitando = True
+    self.numeros = ""
     
-    sg.theme('Reddit')
-    self.layout = [
-      [sg.Text(' ')],# Onde mostram os numeros que serão calculados
-      [sg.Button('7'),sg.Button('8'),sg.Button('9'),sg.Button('/')],# 7,8,9,/
-      [sg.Button('4'),sg.Button('5'),sg.Button('6'),sg.Button('x')],# 4,5,6,x
-      [sg.Button('1'),sg.Button('2'),sg.Button('3'),sg.Button('-')],# 1,2,3,-
-      [sg.Button('0'),sg.Button('='),sg.Button('+')]# 0,=,+
-    ]
   def Iniciar(self):
+    #Layout
+    
+    self.layout = [
+      [sg.Text('Calculadora UMP-2000', justification='right', font=("Helvetica", 25))],
+      [sg.Output(size=(25,2), font=("Helvetica", 25))],# Onde mostram os numeros que serão calculados
+      [sg.Button('7', size=(5, 2),font=("Helvetica", 25)),sg.Button('8', size=(5, 2),font=("Helvetica", 25)),sg.Button('9', size=(5, 2),font=("Helvetica", 25)),sg.Button('/', size=(5, 2),font=("Helvetica", 25))],# 7,8,9,/
+      [sg.Button('4', size=(5, 2),font=("Helvetica", 25)),sg.Button('5', size=(5, 2),font=("Helvetica", 25)),sg.Button('6', size=(5, 2),font=("Helvetica", 25)),sg.Button('x', size=(5, 2),font=("Helvetica", 25))],# 4,5,6,x
+      [sg.Button('1', size=(5, 2),font=("Helvetica", 25)),sg.Button('2', size=(5, 2),font=("Helvetica", 25)),sg.Button('3', size=(5, 2),font=("Helvetica", 25)),sg.Button('-', size=(5, 2),font=("Helvetica", 25))],# 1,2,3,-
+      [sg.Button('0', size=(5, 2),font=("Helvetica", 25)),sg.Button('=', size=(5, 2),font=("Helvetica", 25)),sg.Button('C', size=(5, 2),font=("Helvetica", 25)),sg.Button('+', size=(5, 2),font=("Helvetica", 25))]# 0,=,+,C
+    ]
+ 
     # Criando a janela da Calculadora
     self.janela = sg.Window('Calculadora',layout=self.layout)
-    self.eventos, self.valores = self.janela.read()
-    try:  
-        if self.eventos == '9':
-          self.numeros = '9'
-        elif self.eventos == '8':
-          self.numeros = '8'
-        elif self.eventos == '7':
-          self.numeros = '7'
-        elif self.eventos == '6':
-          self.numeros = '6'
-        elif self.eventos == '5':
-          self.numeros = '5'
-        elif self.eventos == '4':
-          self.numeros = '4'
-        elif self.eventos == '3':
-          self.numeros = '3'
-        elif self.eventos == '2':
-          self.numeros = '2'
-        elif self.eventos == '1':
-          self.numeros = '1'
-        elif self.eventos == '0':
-          self.numeros = '0'
-        elif self.eventos == '-':
-          self.numeros = '-'
-        elif self.eventos == '+':
-          self.numeros = '+'
-        elif self.eventos == '/':
-          self.numeros = '/'
-        elif self.eventos == '=':
-          self.numeros = '='
+    try:
+      while True:
+        self.eventos, self.valores = self.janela.Read() #recebendo valores:
+        while self.digitando == True:
+          if self.eventos != '.':
+            self.numeros=str(self.numeros)+str(self.eventos)
+            print(self.numeros)
+            break
+          else:
+            print('error')
+            break
     except:
-            print('Ocorreu um erro ao receber sua resposta')
+      print('Ocorreu um erro ao receber sua resposta')
+      self.Iniciar()
 
 Calc = Calculadora()
 Calc.Iniciar()
